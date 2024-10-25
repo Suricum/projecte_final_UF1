08fa223118b78643be429266f71ea122f2b159b0
Rafi <rafaelrobles.alum@insestatut.cat>
2024-10-25 16:20:33 +0200

    bbdd biblioteca

--- bbdd.sql
+++ bbdd.sql
@@ -15,6 +15,9 @@ CREATE TABLE llibres    (
     id INT increment PRIMARY KEY,
     nom VARCHAR(),
     autor VARCHAR(),
-    
+    isbn VARCHAR(),
+    stock INT DEFAULT 0,
+    biblioteca_id INT,
+    FOREIGN KEY (biblioteca_id) REFERENCES biblioteques(id) ON DELETE SET NULL
 
 )
